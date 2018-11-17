de6d8ee84fc8610f54dc797104f53c4c15053885
sbanga16k <saurabh.banga94@gmail.com>
2018-11-16 19:00:17 -0500

    Initial camera movement complete!

--- Camera_test/Camera_test/CameraObject.cpp
+++ Camera_test/Camera_test/CameraObject.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 // Default constructor
 CameraObject::CameraObject() {
-	Initialize();
+	initialize();
 }
 
 // Initializes camera object
@@ -68,79 +68,120 @@ void CameraObject::setCameraTransform()
 	glMatrixMode(GL_MODELVIEW);		// Indicates that program will set the transformation matrix
 	glLoadIdentity();				// Resets the projection matrix
 
-	glRotated(-b, 0.0, 0.0, 1.0);
-	glRotated(-p, 1.0, 0.0, 0.0);
-	glRotated(-h, 0.0, 1.0, 0.0);
-	glTranslated(-x, -y, -z);
+	// Executes the transformation
+	glRotated(-bank, 0.0, 0.0, 1.0);
+	glRotated(-pitch, 1.0, 0.0, 0.0);
+	glRotated(-heading, 0.0, 1.0, 0.0);
+	glTranslated(-camX, -camY, -camZ);
 }
 
 
-// Moves the camera in accordance with the keypress
-void CameraObject::moveCamera(int rotInc, int translInc)
+// Rotates & Moves the camera in accordance with the keypress
+void CameraObject::moveCamera(double rotInc, double translInc)
 {
-	double compX, compY, compZ;		// Stores components of unit vector in current direction along each axis
-	this->getEndComponents(compX, compY, compZ);
+	// Stores components of unit vector in current North direction along each axis
+	double compX, compY, compZ;
+	this->getForwardComponents(compX, compY, compZ);
 
-	// Rotates camera acc. to arrow key press in 1 degree increments
+	// Stores components of unit vector in current East direction along each axis
+	double compRightX, compRightZ;
+	this->getRotComponents(compX, compZ, compRightX, compRightZ);
+
+	//// Rotates camera acc. to arrow key press in 1 degree increments
+	// Right-left rotation
 	if (FsGetKeyState(FSKEY_RIGHT)) {
-		heading += rotInc;
-	}
-	else if (FsGetKeyState(FSKEY_LEFT)) {
 		heading -= rotInc;
 	}
-	else if (FsGetKeyState(FSKEY_UP)) {
-		pitch += rotInc;
+	else if (FsGetKeyState(FSKEY_LEFT)) {
+		heading += rotInc;
+	}
+
+	// Allows rotation right-left uncapped
+	// But angle clipped to be in the range of [-180,180]
+	if (heading > 360 || heading < -360)
+		heading = (heading > 360)*(heading - 360) + (heading < -360)*(heading + 360);
+	
+	// Up-down rotation
+	// Upward rot capped at 10 deg, downward rot capped at -20 deg
+	if (FsGetKeyState(FSKEY_UP)) {
+		if (pitch < 10)
+			pitch += rotInc;
 	}
 	else if (FsGetKeyState(FSKEY_DOWN)) {
-		pitch -= rotInc;
+		if (pitch > -20)
+			pitch -= rotInc;
 	}
 
-	// Translates camera acc. to WASD key press
+	//// Translates camera acc. to WASD key press
+	// Moves in North direction
 	else if (FsGetKeyState(FSKEY_W)) {
-		camX += compX * translInc;
-		camY += compY * translInc;
+		camX += -compX * translInc;
 		camZ += compZ * translInc;
 	}
-	else if (FsGetKeyState(FSKEY_A)) {
-		camX -= compX * translInc;
+
+	// Moves in East direction
+	else if (FsGetKeyState(FSKEY_D)) {
+		camX += compRightX * translInc;
+		camZ += compRightZ * translInc;
 	}
+
+	// Moves in West direction
+	else if (FsGetKeyState(FSKEY_A)) {
+		camX -= compRightX * translInc;
+		camZ -= compRightZ * translInc;
+	}
+
+	// Moves in South direction
 	else if (FsGetKeyState(FSKEY_S)) {
-		camX -= compX * translInc;
-		camY -= compY * translInc;
+		camX -= -compX * translInc;
 		camZ -= compZ * translInc;
 	}
-	else if (FsGetKeyState(FSKEY_D)) {
-		camX += compX * translInc;
-	}
-	else if (FsGetKeyState(FSKEY_A)) {
-		camX +=  * 0.5;
-	}
-	else if (FsGetKeyState(FSKEY_D)) {
-		camX -=  * 0.5;
-	}
+	
 }
 
 
-// Sets the input args to components of unit vector in current direction along each axis
-void CameraObject::getComponents(double &compX, double &compY, double &compZ)
+// Sets the input args to components of unit vector in current forward direction along each axis
+void CameraObject::getForwardComponents(double &compX, double &compY, double &compZ)
 {
-	compX = cos(pitch)*-sin(heading);		// (Projection of end of FoV on XZ plane) x (its component along X axis)
-	compY = sin(pitch);					// Projection of end of FoV on Y axis
-	compZ = cos(pitch)*-cos(heading);		// (Projection of end of FoV on XZ plane) x (its component along Z axis)
+	double p = pitch * PI / 180.;
+	double h = heading * PI / 180.;
+	compX = cos(p)*sin(h);	// (Projection of end of FoV on XZ plane) x (component along X axis)
+	compY = sin(p);					// Projection of end of FoV on Y axis
+	compZ = cos(p)*-cos(h);	// (Projection of end of FoV on XZ plane) x (component along Z axis)
 }
 
+
+// Sets input args to components of vector along each axis rotated by specified angle (in degrees CCW)
+void CameraObject::getRotComponents(double compX, double compZ, double & rotX, double & rotZ, double rotAngle)
+{
+	double theta = rotAngle * PI / 180.;
+	rotX = compX * cos(theta) - compZ * sin(theta);
+	rotZ = compX * sin(theta) + compZ * cos(theta);
+}
 
 // Prints values of camera's x,y,z coords & h,p,b angles (for debugging)
-char* CameraObject::printVals()
+char* CameraObject::printVals(bool debug)
 {
 	string temp;
+	double headingDisp;
 
-	temp = " x=" + to_string(x);
-	temp += " y=" + to_string(y);
-	temp += " z=" + to_string(z);
-	temp += " h=" + to_string(h);
-	temp += " p=" + to_string(p);
-	temp += " b=" + to_string(b);
+	if (heading > 180)
+		headingDisp = -180 + (heading - 180);
+	else if (heading < -180)
+		headingDisp = 180 + (heading + 180);
+	else
+		headingDisp = heading;
+
+	if (debug) {
+		temp = " x=" + to_string(camX);
+		temp += " y=" + to_string(camY);
+	}
+	temp += " z=" + to_string(camZ);
+	temp += " h=" + to_string(headingDisp);
+	if (debug) {
+		temp += " p=" + to_string(pitch);
+		temp += " b=" + to_string(bank);
+	}
 
 	char* result = new char[temp.size() + 1];
 	strcpy_s(result, temp.size() + 1, temp.c_str());
--- Camera_test/Camera_test/CameraObject.h
+++ Camera_test/Camera_test/CameraObject.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include<limits>
+
 const double PI = 3.14159265359;
-const double INF = std::numeric_limits<double>::max();
+const double INF = std::numeric_limits<double>::infinity();
 
 class CameraObject
 {
@@ -15,15 +17,17 @@ public:
 	void initialize();				// Initializes camera object
 
 	// Sets camera origin as per global coord system
-	void setCameraCoords(double x = INF, double y = INF, double z = INF);
-	void setCameraAngles(double h = INF, double p = INF);				// Sets camera angles
+	void setCameraCoords(double x = 0., double y = 0., double z = 0.);
+	void setCameraAngles(double h = 0., double p = 0.);				// Sets camera angles
 	void getCameraParams(double &x, double &z, double &h, double &p);	// Gets camera params
 
 	void setCameraProjection();	// Sets projection mtx for calc. screen coords using camera's coord system
 	void setCameraTransform();	// Sets mtx for transformation from global coord to camera's coord system
 
-	void moveCamera(int rotInc = 1, int translInc = 5);	// Moves the camera in accordance with the keypress
-	// Sets input args to components of unit vector in current direction along each axis
-	void getComponents(double &endX, double &endY, double &endZ);
-	char* printVals(char *result);		// Prints values of camera's x,y,z coords & h,p,b angles (for debugging)
+	void moveCamera(double rotInc = 1., double translInc = 0.5);	// Moves the camera in accordance with the keypress
+	// Sets input args to components of unit vector in current forward direction along each axis
+	void getForwardComponents(double &endX, double &endY, double &endZ);
+	// Sets input args to components of rotated vector along each axis
+	void getRotComponents(double compX, double compZ, double &rotX, double &rotZ, double rotAngle = 90);
+	char* printVals(bool debug = false);		// Prints values of camera's z coord & h angles (for visual aid)
 };
